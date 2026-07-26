90644215852ab6f9a199ea3c37d3fa7585f2e4d2
Pritinagi <Pritinagig9494@gmail.com>
2026-07-26 18:45:50 +0530

    feat: create category and product tables

---
+++ Database/02_Create_Table.sql
@@ -0,0 +1,34 @@
+USE RetailOperationalDatabase;
+GO
+/*==========--Category_Table--==========*/
+CREATE TABLE dbo.Categories
+(
+	Category_Id INT IDENTITY(101,1) ,
+	Category_Name VARCHAR(50) NOT NULL,
+
+	CONSTRAINT UQ_Category_Name
+	UNIQUE (Category_Name),
+
+	CONSTRAINT Pk_Category_Id
+	PRIMARY KEY (Category_Id)
+
+);
+
+/*==========--Product_Table--==========*/
+CREATE TABLE dbo.Products
+(
+	Product_Id INT IDENTITY(201,1),
+	Product_Name VARCHAR(50) NOT NULL,
+	Product_Price DECIMAL(10,2) NOT NULL,
+	Category_Id INT NOT NULL ,
+
+	CONSTRAINT Pk_Product_Id
+	PRIMARY KEY (Product_Id),
+
+	CONSTRAINT UQ_Product_Category_Name
+	UNIQUE (Category_Id,Product_Name),
+
+	CONSTRAINT Fk_Product_Category
+	FOREIGN KEY (Category_Id)
+	REFERENCES dbo.Categories(Category_Id)
+)

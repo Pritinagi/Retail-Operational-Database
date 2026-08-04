a21b752d73927eadd57d7545317abf5cc6988d74
Pritinagi <Pritinagig9494@gmail.com>
2026-08-04 14:04:47 +0530

    Add lowest revenue products analysis query

---
+++ Queries/Products/05_Lowest_Revenue_Products.sql
@@ -0,0 +1,12 @@
+/*==========-Purpose - Show the bottom 10 products based on total revenue generated, including products that have never been sold. -==========*/
+
+
+SELECT TOP 10
+	p.Product_Id,
+	p.Product_Name,
+	SUM(oi.unit_price*oi.Quantity) as Revenue
+FROM dbo.Products as p
+LEFT JOIN dbo.OrderItems as oi
+ON p.Product_Id=oi.Product_Id
+GROUP BY p.Product_Id,p.Product_Name
+ORDER BY Revenue ASC

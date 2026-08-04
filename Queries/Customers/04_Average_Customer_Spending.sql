4d6c02ffdf0b422de3a98a45bceb597069864933
Pritinagi <Pritinagig9494@gmail.com>
2026-08-04 13:21:11 +0530

    Add average customer spending analysis query

---
+++ Queries/Customers/04_Average_Customer_Spending.sql
@@ -0,0 +1,12 @@
+/*==========-Purpose - Calculate the average amount spent by each customer across all of their orders -==========*/
+
+
+SELECT
+	Customer_Name,
+	c.customer_id,
+	avg(Total_Amount) as AverageAmount
+FROM dbo.Customers as c
+INNER JOIN dbo.Orders as o
+ON c.Customer_Id=o.Customer_Id
+GROUP BY Customer_Name,c.Customer_Id
+ORDER BY 

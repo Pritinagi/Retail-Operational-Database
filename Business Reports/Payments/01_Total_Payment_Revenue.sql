faf866a75713f3e9c5d40db912b123dceac36653
Pritinagi <Pritinagig9494@gmail.com>
2026-08-05 23:10:46 +0530

    Complete business reports module

--- Business Reports/Payments/01_Total_Payment_Revenue.sql
+++ Business Reports/Payments/01_Total_Payment_Revenue.sql
@@ -1,5 +1,10 @@
 /*==========-Purpose - Calculate the total revenue successfully received through payments. -==========*/
 
 
-SELECT * FROM dbo.Payments as p
-SELECT * FROM dbo.Orders as o
+SELECT 
+	SUM(o.total_amount) RevenueByPayments
+FROM dbo.Payments as p
+
+INNER JOIN dbo.Orders as o
+ON p.order_id=o.order_id
+
